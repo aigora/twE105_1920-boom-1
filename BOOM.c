1345dbffe1e883016d2fa9117776d1676d3b7af5
hectordelsaz <47813285+hectordelsaz@users.noreply.github.com>
2020-03-19 14:35:43 +0100

    Programa inicialpara un jugador mas claro

--- BOOM.c
+++ BOOM.c
@@ -3,7 +3,6 @@
 int main ()
 {
 printf ("Bienvenido a BOOM!!\n Elige la modalidad a la que deseas jugar teniendo en cuenta que:\n -si pulsas 1 jugaras en modo individual.\n -si pulsas 2 jugaras en modo multijugador.\n -pulsa cualquier otra tecla para salir del juego\n");
-<<<<<<< HEAD
 char option,name_1[20],name_2[20],tema;
 scanf ("%c", &option);
 switch (option)
@@ -13,7 +12,7 @@ switch (option)
 	scanf ("%s", &name_1);
 	printf (" PRIMERA PARTE DEL JUEGO:\nINSTRUCCIONES: %s corta el cable de todas las respuestas exceto de la que creas que es la correcta.\n ", name_1);
 	
-	printf ( "BOMBA BONUS:\nINSTRUCCIONES: %s elije un tema teniendo en cuenta que:\n.1 pertenece a DEPORTES.\n.2 pertenece a HISTORIA\n.3 iremos incluyendo más.\n Posteriormente, selecciona las 5 respuestas correctas.\n", name_1);
+	printf ( "BOMBA BONUS:\nINSTRUCCIONES: %s elije un tema teniendo en cuenta que:\n.1 pertenece a DEPORTES.\n.2 pertenece a HISTORIA\n.3 iremos incluyendo mas.\n Posteriormente, selecciona las 5 respuestas correctas.\n", name_1);
 	scanf (" %c", &tema);
 		switch (tema)
 		{
@@ -26,7 +25,7 @@ switch (option)
 			break;
 			
 			case '3':
-			printf ("IREMOS INCLUYENDO MÁS\n");
+			printf ("IREMOS INCLUYENDO MaS\n");
 			break;
 			
 			default:
@@ -35,35 +34,16 @@ switch (option)
 		}
 	
 	printf ( "BOMBA FINAL\n");
-=======
-char option,name_1[20],name_2[20];
-scanf ("%c",&option);
-switch (option)
-{
-	case '1': // CASO INDIVIDUAL. El juego será de un único jugador, o grupo, dando una única solución//
-	printf ("Has elegido el modo individual. Por favor introduce el nombre del jugador\n");
-	scanf ("%s", &name_1);
->>>>>>> 8772f8218c03b6735be8f9c27db3c08c20cfa5d7
 	break;
 	
 	
 	case '2': // CASO MULTIJUGADOR. Se trata de dos grupos, pero dando una única solución por grupo.//
 	printf ("Has elegido el modo multijugador. Por favor introduce el nombre del jugador 1.\n");
-<<<<<<< HEAD
 	scanf ("%s",&name_1);
 	printf ("Ahora introduce el nombre del jugador 2.\n");
 	scanf ("%s", &name_2 );
-	break;
 	
-	
-=======
-	scanf ("%s", &name_1);
-	printf ("Ahora introduce el nombre del jugador 2.\n");
-	scanf ("%s", &name_2);
-	break;
-	
->>>>>>> 8772f8218c03b6735be8f9c27db3c08c20cfa5d7
-	default:
+	default :
 	printf ("HASTA PRONTO.\n");
 	break;
 }
