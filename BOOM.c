6d2e8a1e83d67b248c741d0c670bcc31062e85e2
hectordelsaz <47813285+hectordelsaz@users.noreply.github.com>
2020-03-19 15:42:14 +0100

    Update BOOM.c

--- BOOM.c
+++ BOOM.c
@@ -3,7 +3,7 @@
 int main ()
 {
 printf ("Bienvenido a BOOM!!\n Elige la modalidad a la que deseas jugar teniendo en cuenta que:\n -si pulsas 1 jugaras en modo individual.\n -si pulsas 2 jugaras en modo multijugador.\n -pulsa cualquier otra tecla para salir del juego\n");
-char option,name_1[20],name_2[20],tema;
+char option,name_1[20],name_2[20],tema,dinero_1,dinero_2;
 scanf ("%c", &option);
 switch (option)
 {
@@ -13,7 +13,7 @@ switch (option)
 	printf (" PRIMERA PARTE DEL JUEGO:\nINSTRUCCIONES: %s corta el cable de todas las respuestas exceto de la que creas que es la correcta.\n ", name_1);
 	
 	printf ( "BOMBA BONUS:\nINSTRUCCIONES: %s elije un tema teniendo en cuenta que:\n.1 pertenece a DEPORTES.\n.2 pertenece a HISTORIA\n.3 iremos incluyendo mas.\n Posteriormente, selecciona las 5 respuestas correctas.\n", name_1);
-	scanf (" %c", &tema);
+	scanf (" %c", &tema); // En este caso la bomba bonus sería para conseguir 15segundos extra para la bomba final. Una vez juguemos esta, el dinero que llevemos hasta entonces estaría asegurado//
 		switch (tema)
 		{
 			case '1': 
@@ -33,7 +33,8 @@ switch (option)
 			break;
 		}
 	
-	printf ( "BOMBA FINAL\n");
+	printf ("BOMBA FINAL.\n"); //Consiste en contestar durante un minuto(sin contar si se ha conseguido bonus) preguntas de dos opciones. Cada acierto será un +300, cada error -100//
+	printf ( "BOMBA PARA EL BOTE\n");
 	break;
 	
 	
@@ -42,8 +43,63 @@ switch (option)
 	scanf ("%s",&name_1);
 	printf ("Ahora introduce el nombre del jugador 2.\n");
 	scanf ("%s", &name_2 );
+	printf (" PRIMERA PARTE DEL JUEGO PARA EL JUGADOR 1:\nINSTRUCCIONES: %s corta el cable de todas las respuestas excepto de la que creas que es la correcta.\n ", name_1);
+	printf (" PRIMERA PARTE DEL JUEGO PARA EL JUGADOR 1:\nINSTRUCCIONES: %s corta el cable de todas las respuestas excepto de la que creas que es la correcta.\n ", name_2);
 	
-	default :
+	printf ( "BOMBA BONUS PARA JUGADOR 1:\nINSTRUCCIONES: %s elije un tema teniendo en cuenta que:\n.1 pertenece a DEPORTES.\n.2 pertenece a HISTORIA\n.3 iremos incluyendo mas.\n Posteriormente, selecciona las 5 respuestas correctas.\n", name_1);
+	scanf (" %c", &tema);// En este caso la bomba bonus sería para conseguir 15segundos extra para la bomba final para el jugador 1. Una vez juguemos esta, el dinero que llevemos hasta entonces estaría asegurado. Pasa a la BOMBA PARA EL BOTE quien más dinero tenga//
+		switch (tema)
+		{
+			case '1': 
+			printf ("DEPORTES\n");
+			break;
+			
+			case '2':
+			printf ("HISTORIA\n");
+			break;
+			
+			case '3':
+			printf ("IREMOS INCLUYENDO MaS\n");
+			break;
+			
+			default:
+			printf ("Elija bien el tema.\n");
+			break;
+		}
+	printf ( "BOMBA BONUS PARA JUGADOR 2:\nINSTRUCCIONES: %s elije un tema teniendo en cuenta que:\n.1 pertenece a DEPORTES.\n.2 pertenece a HISTORIA\n.3 iremos incluyendo mas.\n Posteriormente, selecciona las 5 respuestas correctas.\n", name_2);
+	scanf (" %c", &tema); // En este caso la bomba bonus sería para conseguir 15segundos extra para la bomba final para el jugador 2. Una vez juguemos esta, el dinero que llevemos hasta entonces estaría asegurado. Pasa a la BOMBA PARA EL BOTE quien más dinero tenga//
+		switch (tema)
+		{
+			case '1': 
+			printf ("DEPORTES\n");
+			break;
+			
+			case '2':
+			printf ("HISTORIA\n");
+			break;
+			
+			case '3':
+			printf ("IREMOS INCLUYENDO MaS\n");
+			break;
+			
+			default:
+			printf ("Elija bien el tema.\n");
+			break;
+		}
+		
+		printf ("BOMBA FINAL PARA %s\n", name_1);
+		printf ("BOMBA FINAL PARA %s\n", name_2);
+		
+		if (dinero_1>dinero_2)
+		printf ("Como %s ha obtenido %i y %s ha obtenido %i, pasa a jugar la BOMBA PARA EL BOTE el jugador %s.\n",name_1,dinero_1,name_2,dinero_2,name_1);
+		else if (dinero_1=dinero_2)
+		printf ("Pregunta de desempate\n");
+		else 
+		printf ("Como %s ha obtenido %i y %s ha obtenido %i, pasa a jugar la BOMBA PARA EL BOTE el jugador %s.\n",name_2,dinero_2,name_1,dinero_1,name_2);
+		
+	printf("BOMBA PARA EL BOTE.\n");//como he dicho solo la jugará el que más dinero haya conseguido hasta la bomba anterior.Consistirá en contestar 10 preguntas a lo largo de 2 minutos//
+	
+	default:
 	printf ("HASTA PRONTO.\n");
 	break;
 }
