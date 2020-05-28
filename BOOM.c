fd97e7e8ed158158c3a9929db13ba7c94d5d68ae
oscararrigo <47812942+oscararrigo@users.noreply.github.com>
2020-05-28 21:07:00 +0100

    Librerias creadas

--- BOOM.c
+++ BOOM.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include "funciones.h"
+#include "Libreriafunciones.h"
 
-int temporizador1 ();
+/*int temporizador1 ();
 int faseinicial ();
 int PyR1 ();
 int PyR34 ();
 int PyRfinal ();
 int PyRbonus ();
-int acumulador ();
+int acumulador ();*/
 
 int main ()
 {
@@ -91,6 +91,5 @@ int main ()
 
 return 0;
 }
-
 
 
---
+++ prototipofunc.h
@@ -0,0 +1,7 @@
+int temporizador1 ();
+int faseinicial ();
+int PyR1 ();
+int PyR34 ();
+int PyRfinal ();
+int PyRbonus ();
+int acumulador ();
