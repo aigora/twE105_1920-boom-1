be9c0724652ca26cab4cedecaded1ff8529f4538
hectordelsaz <h.delsaz@alumnos.upm.es>
2020-05-28 17:44:14 +0200

    Funcion preguntas y respuestas bomba bonus
    Incluyo funcion preguntas y respuestas bomba bonus

--- BOOM.c
+++ BOOM.c
@@ -5,6 +5,8 @@ int faseinicial ();
 int PyR1 ();
 int PyR34 ();
 int PyRfinal ();
+int PyRbonus ();
+
 
 int main ()
 {
@@ -1005,3 +1007,380 @@ if (rline == 30){
 getchar();
 	return (s);
 }
+
+int PyRbonus (){
+	FILE   *fp7;
+FILE   *fp8;
+char   buf[200];
+int    flines;    /* numero de lineas en el archivo */
+int    rline;     /* la linea aleatoria para leer */
+int    i;
+int    resp;
+int    k;
+int    s;
+ 
+s=0;
+srand(time(NULL));
+if ( ! ( fp7 = fopen("PreguntasBBonus.txt", "r")) )
+   {
+   printf("Error al abrir el archivo\n");
+   }
+ 
+for ( flines = 0 ; fgets(buf, sizeof(buf), fp7) ; ++flines);
+ 
+if ( flines == 0 )
+   {
+   printf("El archivo esta vacio\n");
+   }
+ 
+rline = rand() % (flines+1);
+ 
+rewind(fp7);
+ 
+for ( i = 0 ; i < rline  ; ++i )
+    fgets(buf, sizeof(buf), fp7);
+printf("%s\n", buf);
+
+//printf("%i",rline);
+
+
+
+//_____________
+
+
+if ( ! ( fp8 = fopen("RespuestasBBonus.txt", "r")) )
+   {
+   printf("\nError al abrir el archivo\n");
+   return(0);
+   }
+   
+for ( i = 0 ; i < rline  ; ++i )
+    fgets(buf, sizeof(buf), fp8);
+printf("%s", buf);
+
+scanf("%i",&resp);
+
+
+if (rline == 1){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 2){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 3){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	
+	}
+}
+if (rline == 4){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 5){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 6){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 7){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 8){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 9){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	
+	}
+}
+if (rline == 10){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 11){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 12){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 13){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	
+	}
+}
+if (rline == 14){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 15){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+
+if (rline == 16){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 17){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 18){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	
+	}
+}
+if (rline == 19){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 20){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 21){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 22){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 23){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 24){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	
+	}
+}
+if (rline == 25){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 26){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 27){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 28){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	
+	}
+}
+if (rline == 29){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 30){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 31){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 32){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 33){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+	
+	}
+}
+if (rline == 34){
+	switch (resp){
+		case 1: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("incorrecto\n");
+				s=2;
+	}
+}
+if (rline == 35){
+	switch (resp){
+		case 2: printf("Correcto\n");
+				s=1;
+		break;
+		default: printf("Incorrecto\n");
+				s=2;
+		}
+		}
